49a9d573456a2243c166bc4b012498b192dba4db
archzaq <129307974+archzaq@users.noreply.github.com>
2026-06-01 15:25:52 -0500

    created repo

---
+++ binary/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Author: Zac Reeves
+ * Created: 06-01-26
+ * Updated: 06-01-26
+ * Desc: This program's purpose is to convert a binary string to decimal
+ * as well as convert decimal string to binary
+ */
+
+int main() {
+    printf("Beginning binary conversion\n");
+    return 0;
+}
