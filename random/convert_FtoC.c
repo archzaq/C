e6cf46602b3c6d63f90b27381c729af2bad4405d
archzaq <129307974+archzaq@users.noreply.github.com>
2026-06-05 09:47:53 -0500

    changed how to set variables from hard coded to dynamic

--- random/convert_FtoC.c
+++ random/convert_FtoC.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Author: Zac Reeves
  * Created: 06-01-26
- * Updated: 06-01-26
+ * Updated: 06-05-26
+ * Version: 1.1
  * Desc: Convert Fahrenheit to Celsius
  */
 
-int main() {
+void usage(char *argv[]) {
+    printf("\nUsage: %s <min number> <max number> <step (optional)>\n\n", argv[0]);
+}
+
+int main(int argc, char *argv[]) {
     int fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
-        fahr = fahr + step;
+    if (argc < 3) {
+        usage(&argv[0]);
+        return 1;
+    } else if (argc == 3) {
+        step = 1;
+    } else {
+        step = atoi(argv[3]);
     }
+
+    lower = atoi(argv[1]);
+    upper = atoi(argv[2]);
+    if (lower < upper) {
+        fahr = lower;
+        while (fahr <= upper) {
+            celsius = 5 * (fahr-32) / 9;
+            printf("%d\t%d\n", fahr, celsius);
+            fahr = fahr + step;
+        }
+    } else {
+        printf("\nError: The Minimum must be lower than the Maxmimum\n");
+        usage(&argv[0]);
+    }
+
     return 0;
 }
