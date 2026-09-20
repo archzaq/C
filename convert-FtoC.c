ad4ca5125cdb2911d79a3e8412616fa5fba03ac6
archzaq <129307974+archzaq@users.noreply.github.com>
2026-09-20 00:59:14 -0500

    added more checking for input

--- convert-FtoC.c
+++ convert-FtoC.c
@@ -3,8 +3,8 @@
 
 /* Author: Zac Reeves
  * Created: 06-01-26
- * Updated: 06-05-26
- * Version: 1.2
+ * Updated: 09-20-26
+ * Version: 1.3
  * Desc: Convert Fahrenheit to Celsius
  *       Takes min, max, and step from command line
  */
@@ -17,13 +17,22 @@ int main(int argc, char *argv[]) {
     int fahr, celsius;
     int lower, upper, step;
 
+    // Ensure Min and Max are provided, step optional
     if (argc < 3) {
+        printf("\nProvide at least two arguments!\n");
         usage(&argv[0]);
         return 1;
     } else if (argc == 3) {
         step = 1;
     } else if (argc == 4) {
         step = atoi(argv[3]);
+        if (step <= 0) {
+            printf("\nStep is too low!\n");
+            usage(&argv[0]);
+            return 1;
+        } else {
+            step = atoi(argv[3]);
+        }
     } else {
         printf("\nToo many arguments!\n");
         usage(&argv[0]);
@@ -40,7 +49,7 @@ int main(int argc, char *argv[]) {
             fahr = fahr + step;
         }
     } else {
-        printf("\nError: The Minimum must be lower than the Maxmimum\n");
+        printf("\nThe Minimum must be lower than the Maxmimum\n");
         usage(&argv[0]);
     }
 
